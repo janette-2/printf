e709794bdffa9aaef65e3417165dec232cc8eefd
Janette Rodriguez Ponce <janerodpon@gmail.com>
2025-11-18 20:04:17 +0100

    18/11/25

--- libftprintf.h
+++ libftprintf.h
@@ -6,7 +6,7 @@
 /*   By: janrodri <janrodri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/11 18:13:51 by janrodri          #+#    #+#             */
-/*   Updated: 2025/11/17 18:42:27 by janrodri         ###   ########.fr       */
+/*   Updated: 2025/11/18 20:03:46 by janrodri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,11 @@ void	ft_check_bytes_count(int *count, int written_bytes);
 
 #endif
 
-/* TO COMPILE THE FILES WITH THE libft USE THIS:
+/* TO COMPILE THE FILES WITH THE libft USE THIS AFTER 'make':
 
-cc -Wall -Werror -Wextra ft_find_percentage.c -Llibft -lft && ./a.out
+ cc -Wall -Werror -Wextra ft_printf.c ft_format.c ft_putchar_fd_count.c 
+ft_putstr_fd_count.c ft_put_pointer_fd.c ft_put_nbr_fd_count.c 
+ft_put_unsigned_nbr_fd.c ft_put_hex_nbr_fd.c ft_put_HEX_nbr_fd.c 
+ft_put_percentage_fd.c ft_check_bytes_count.c -Llibft -lft && ./a.out
+
 */
